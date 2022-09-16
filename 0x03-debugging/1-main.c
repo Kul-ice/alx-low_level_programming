d67fb856fb0d8fb8add27c6c0f345f2f197cbbdf
Kul-ice <ikuereyecourage@gmail.com>
2022-09-15 19:55:14 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
 	/*
@@ -17,9 +18,10 @@ int main(void)
 	 * {
 	 * putchar(i);
 	 * }
+	 *
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
 
