d723c0ecd984384a290add1c7b46ca2aad328695
Kul-ice <ikuereyecourage@gmail.com>
2022-10-27 14:59:19 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
@@ -7,7 +6,6 @@
  *
  * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
@@ -18,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	ui = 0;
 
-	for (len = 0; b[len] != '\0'; len++)	
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
